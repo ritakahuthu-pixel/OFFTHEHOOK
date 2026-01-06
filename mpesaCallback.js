de438f8d81a9091e850c967fa02dd19e989f3332
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-01-06 22:42:19 +0300

    Refactor MPESA callback to use Express router

--- mpesaCallback.js
+++ mpesaCallback.js
@@ -1,5 +1,14 @@
+const express = require("express");
+const router = express.Router();
 
-exports.handleCallback = (req, res) => {
-  console.log("MPESA CALLBACK:", JSON.stringify(req.body, null, 2));
-  res.json({ ResultCode: 0, ResultDesc: "Accepted" });
-};
+router.post("/", (req, res) => {
+  console.log("📩 MPESA CALLBACK RECEIVED");
+  console.log(JSON.stringify(req.body, null, 2));
+
+  res.json({
+    ResultCode: 0,
+    ResultDesc: "Accepted"
+  });
+});
+
+module.exports = router;
