21fc2be968da172f44ecfe166b60b31baf0f8fbc
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-01-06 20:39:38 +0300

    Refactor server.js to simplify listen log

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 require("dotenv").config();
 const express = require("express");
+
 const payments = require("./routes/payments");
 const { handleCallback } = require("./callbacks/mpesaCallback");
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -15,7 +16,4 @@ app.use("/api/payments", payments);
 app.post("/api/mpesa/callback", handleCallback);
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
