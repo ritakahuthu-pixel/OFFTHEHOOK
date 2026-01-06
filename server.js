21643d71f9c0ec7637817a57c2e67621e81a77cd
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-01-06 18:22:20 +0300

    Refactor server.js to improve server startup logging

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-
 require("dotenv").config();
 const express = require("express");
 const payments = require("./routes/payments");
 const { handleCallback } = require("./callbacks/mpesaCallback");
+
 const app = express();
+
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.use("/api/payments", payments);
 app.post("/api/mpesa/callback", handleCallback);
-app.listen(process.env.PORT, () => console.log("Server running"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
