db43c40d6f2bec2fc16803541366e70e98df9d9b
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-01-06 23:02:12 +0300

    Refactor getAccessToken function for clarity

--- auth.js
+++ auth.js
@@ -1,12 +1,19 @@
-
 const axios = require("axios");
-exports.getAccessToken = async () => {
-  const auth = Buffer.from(
-    `${process.env.MPESA_CONSUMER_KEY}:${process.env.MPESA_CONSUMER_SECRET}`
-  ).toString("base64");
-  const res = await axios.get(
+
+const getAccessToken = async () => {
+  const consumerKey = process.env.MPESA_CONSUMER_KEY;
+  const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
+
+  const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64");
+
+  const { data } = await axios.get(
     "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
-    { headers: { Authorization: `Basic ${auth}` } }
+    {
+      headers: { Authorization: `Basic ${auth}` }
+    }
   );
-  return res.data.access_token;
+
+  return data.access_token;
 };
+
+module.exports = { getAccessToken };
