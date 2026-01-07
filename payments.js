01bd7ebef3cf0446cc4fc455044b1ffefc92ee62
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-01-07 03:38:07 +0300

    Update payments.js

--- payments.js
+++ payments.js
@@ -4,16 +4,10 @@ const stkQuery = require("./stkQuery");
 
 const router = express.Router();
 
-/**
- * Initiate STK Push
- */
 router.post("/stk-push", stkPush);
-
-/**
- * Query STK Push status
- */
 router.post("/stk-query", stkQuery);
 
 module.exports = router;
+
 
 
