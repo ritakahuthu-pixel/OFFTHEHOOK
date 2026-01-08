9f522d0c67bbec12767d5dffde87cffb0da2b3c3
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-01-08 21:17:20 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 const express = require("express");
+const paymentsRoutes = require("./payments");
+
 const app = express();
-
-// 🔴 MUST BE AT THE TOP
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Routes
-const stkPushRoutes = require("./routes/stkPush");
-const stkQueryRoutes = require("./routes/stkQuery");
-
-// Mount routes
-app.use("/payments", stkPushRoutes);
-app.use("/payments", stkQueryRoutes);
-
-// STK CALLBACK (Safaricom hits this)
-app.post("/payment/callback", (req, res) => {
-  console.log("📥 STK CALLBACK RECEIVED");
-  console.log(JSON.stringify(req.body, null, 2));
-
-  res.status(200).json({
-    ResultCode: 0,
-    ResultDesc: "Accepted"
-  });
-});
-
-// Health check
 app.get("/", (req, res) => {
   res.send("OFFTHEHOOK API is live");
 });
 
-// Start server
+app.use("/payments", paymentsRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
