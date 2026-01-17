7562fd227887f0a27ce0d5cbdb1be8e7e788cb5b
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-01-17 15:19:08 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,21 +1,26 @@
-const axios = require("axios");
+import axios from "axios";
 
-async function getAccessToken() {
-  const url =
-    "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+export async function getAccessToken() {
+  try {
+    const consumerKey = process.env.CONSUMER_KEY;
+    const consumerSecret = process.env.CONSUMER_SECRET;
 
-  const auth = Buffer.from(
-    `${process.env.MPESA_CONSUMER_KEY}:${process.env.MPESA_CONSUMER_SECRET}`
-  ).toString("base64");
+    const auth = Buffer.from(
+      `${consumerKey}:${consumerSecret}`
+    ).toString("base64");
 
-  const response = await axios.get(url, {
-    headers: {
-      Authorization: `Basic ${auth}`
-    }
-  });
+    const response = await axios.get(
+      "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+      {
+        headers: {
+          Authorization: `Basic ${auth}`,
+        },
+      }
+    );
 
-  return response.data.access_token;
+    return response.data.access_token;
+  } catch (error) {
+    console.error("Access Token Error:", error.response?.data || error.message);
+    throw new Error("Failed to generate access token");
+  }
 }
-
-module.exports = { getAccessToken };
-
